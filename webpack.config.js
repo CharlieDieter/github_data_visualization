0fa3136b876c5512411090b4ddae1d574b0dd334
Charlie Dieter <charlie.dieter@gmail.com>
2018-03-21 17:16:51 -0400

    Connect to firebase

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify("production")
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === "production" ? prodPlugins : devPlugins
+);
+module.exports = {
+  context: __dirname,
+  entry: "./lib/git_visual_history.js",
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        loader: "babel-loader",
+        query: {
+          presets: ["env", "es2015"]
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
