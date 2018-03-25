a7f645990518d4eb6252dd32e9e6adbe4b1e552f
Charlie Dieter <charlie.dieter@gmail.com>
2018-03-25 09:15:09 -0400

    Add leaf nodes

---
+++ data/images.js
@@ -0,0 +1,26 @@
+var images = {
+  JavaScript:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Badge_js-strict.svg/2000px-Badge_js-strict.svg.png",
+  Java:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png",
+  HTML:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png",
+  CSS:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/2000px-CSS.3.svg.png",
+  Shell: "https://glot.io/static/img/bash.svg.png?etag=5cwCNl16",
+  Python:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2000px-Python-logo-notext.svg.png",
+  Ruby:
+    "https://www.bigbinary.com/assets/services/ror/rubygem-73b83c79780e7e71d4a159177f2cbdb95b07466141beab0380842122d27f4f93.svg",
+  PHP:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/PHP_Logo%2C_text_only.svg/640px-PHP_Logo%2C_text_only.svg.png",
+  C:
+    "https://domboscoonline.com.br/upload/store/281220171514476609linguagem-c.png",
+  "C++":
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/2000px-ISO_C%2B%2B_Logo.svg.png",
+  Go: "https://seeklogo.com/images/G/go-logo-046185B647-seeklogo.com.png",
+  Swift:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Swift_logo.svg/2000px-Swift_logo.svg.png"
+};
+
+module.exports = images;
