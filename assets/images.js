0548e2f142e25b00fe3ec08be0a857efc0ae3878
Charlie Dieter <charlie.dieter@gmail.com>
2018-03-26 10:48:12 -0400

    Convert data to js file

---
+++ assets/images.js
@@ -0,0 +1,82 @@
+var images = {
+  JavaScript:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Badge_js-strict.svg/2000px-Badge_js-strict.svg.png",
+  Java:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png",
+  HTML:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png",
+  CSS:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/2000px-CSS.3.svg.png",
+  Shell: "https://glot.io/static/img/bash.svg.png?etag=5cwCNl16",
+  Python:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2000px-Python-logo-notext.svg.png",
+  Ruby:
+    "https://www.bigbinary.com/assets/services/ror/rubygem-73b83c79780e7e71d4a159177f2cbdb95b07466141beab0380842122d27f4f93.svg",
+  PHP:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/PHP_Logo%2C_text_only.svg/640px-PHP_Logo%2C_text_only.svg.png",
+  C:
+    "https://domboscoonline.com.br/upload/store/281220171514476609linguagem-c.png",
+  "C++":
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/2000px-ISO_C%2B%2B_Logo.svg.png",
+  Go: "https://seeklogo.com/images/G/go-logo-046185B647-seeklogo.com.png",
+  Swift:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Swift_logo.svg/2000px-Swift_logo.svg.png",
+  Makefile: "https://www.gnu.org/graphics/empowered-by-gnu.svg",
+  TypeScript:
+    "https://seeklogo.com/images/T/typescript-logo-B29A3F462D-seeklogo.com.png",
+  SQL: "https://image.flaticon.com/icons/svg/29/29165.svg",
+  "C#": "http://www.codekul.com/images/logo_chash.png",
+  "F#":
+    "https://codeopinion.com/wp-content/uploads/2015/03/FSharp.Azure_.Logo_.png",
+  Clojure: "http://clojurebridge.lispnyc.org/static/images/clojure-logo.png",
+  Kotlin:
+    "http://logos-download.com/wp-content/uploads/2016/10/Kotlin_logo_wordmark.png",
+  Rust:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Rust_programming_language_black_logo.svg/2000px-Rust_programming_language_black_logo.svg.png",
+  "Visual Basic": "http://www.gandgtech.com/images/visualbasic.png",
+  Cobol: "https://hackr.io/tutorials/learn-cobol/logo/cobol_logo",
+  CoffeeScript: "http://www.jeremyschultz.com/img/logos/coffeescript.svg",
+  Matlab: "https://pennwic.files.wordpress.com/2014/02/matlablogo.png",
+  Assembly:
+    "https://hackr.io/tutorials/learn-assembly-language/logo/assembly-language_logo",
+  Perl: "http://www.acornsoftware.net/images/perl-logo.png",
+  "Objective-C":
+    "https://static-s.aa-cdn.net/img/ios/486204866/6e383749be137a9b09bccbefcfc3665d?v=1",
+  Lua:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Lua-logo-nolabel.svg/2000px-Lua-logo-nolabel.svg.png",
+  Groovy:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Groovy-logo.svg/1200px-Groovy-logo.svg.png",
+  "Delphi/ObjectPascal":
+    "https://user-images.githubusercontent.com/6978003/28999656-cb0677aa-7a1c-11e7-975d-25ed65555cc8.png",
+  Erlang:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Erlang_logo.svg/2000px-Erlang_logo.svg.png",
+  Scala:
+    "http://www.unixstickers.com/image/data/stickers/scala/Scala-logoText.sh.png",
+  OCaml:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/OCaml_Logo.svg/800px-OCaml_Logo.svg.png",
+  Hack:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Webysther_20160330_-_Hack_%28language%29.svg/2200px-Webysther_20160330_-_Hack_%28language%29.svg.png",
+  "Node.js":
+    "https://cdn.iconscout.com/public/images/icon/free/png-512/nodejs-logo-36559ec903b263f5-512x512.png",
+  Angular:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1000px-Angular_full_color_logo.svg.png",
+  React:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
+  ".NET Core":
+    "https://cdn0.froala.com/assets/editor/docs/server/meta-social/dotnet-core-12b6094aae01e933196c60b4e87181bf.png",
+  Spring:
+    "http://www.softcodelondon.co.uk/assets/images/expert-logo-set/spring-framework.png",
+  Django: "https://cdn.worldvectorlogo.com/logos/django.svg",
+  Cordova: "https://cdn.worldvectorlogo.com/logos/cordova.svg",
+  TensorFlow:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/2000px-Tensorflow_logo.svg.png",
+  Xamarin:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Xamarin-logo.svg/2000px-Xamarin-logo.svg.png",
+  Spark:
+    "https://camo.githubusercontent.com/7357732177d2f78c60b492621c14c28e69c6088f/68747470733a2f2f63646e2e7261776769742e636f6d2f617765736f6d652d737061726b2f617765736f6d652d737061726b2f66373861313664622f737061726b2d6c6f676f2d74726164656d61726b2e737667",
+  Hadoop:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Hadoop_logo.svg/2000px-Hadoop_logo.svg.png",
+  "Most Loved on Stack Overflow": ""
+};
+
+module.exports = images;
